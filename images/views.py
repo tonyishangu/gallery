2a23013cb552b7e9003c8613817849c2c4bb5669
tonyishangu <tonyishangu@gmail.com>
2020-10-12 08:00:56 +0300

    update views

--- images/views.py
+++ images/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import *
 
 # Create your views here.
 def home(request):
@@ -17,3 +18,23 @@ def home(request):
         return render(request, 'all-images.html', {"name":name,"images":images,"cat":cat })
 
     return render(request,"all-images.html",{"images":images,"location":location,"category":category})
+
+def search_results(request):
+
+    if 'categories' in request.GET and request.GET['categories']:
+        search_images = request.GET.get("categories")
+        searched_images = Image.search_by_category(search_images)
+        message = f"{search_images}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})
+
+def get_image_by_id(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"error.html", {"image":image})
