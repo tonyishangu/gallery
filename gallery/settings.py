9c632fd134fcdc747626a6aefa7971656a2b85f5
tonyishangu <tonyishangu@gmail.com>
2020-10-12 07:50:06 +0300

    change timezone

--- gallery/settings.py
+++ gallery/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'gallery',
         'USER': 'tonnie',
-        
+        'PASSWORD': 'Saige1aw',
     }
 }
 
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
