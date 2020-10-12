64a258b022b70e98ebe365338035df9d0c0cf986
tonyishangu <tonyishangu@gmail.com>
2020-10-12 08:11:27 +0300

    update urls file

--- images/urls.py
+++ images/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns=[
+urlpatterns = [
     url(r'^$', views.home,name='home'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.get_image_by_id,name ='image')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
